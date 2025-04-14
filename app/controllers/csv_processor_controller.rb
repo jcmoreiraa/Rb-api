bcfdb7b57fda22dd0f86cd311191db8dddd7202b
Julio Moreira <juliomoreira@ufba.br>
2025-04-14 11:10:32 -0300

    feat: password for csv

--- app/controllers/csv_processor_controller.rb
+++ app/controllers/csv_processor_controller.rb
@@ -3,10 +3,13 @@ class CsvProcessorController < ApplicationController
   before_action :verificar_senha, only: [:process_csv]
 
   def process_csv
-    file_path = Rails.root.join('db', 'data', 'cu.csv').to_s
+    file_path = Rails.root.join('db', 'data', 'cu.csv.enc').to_s
+    senha_csv = ENV['CSV']
+    data = `openssl enc -d -aes-256-cbc -in #{file_path} -k #{senha_csv}`
+
     arrays = []
 
-    CSV.foreach(file_path, headers: false, col_sep: ",") do |row|
+    CSV.parse(data, headers: false, col_sep: ",") do |row|
       arrays << row 
     end
 
